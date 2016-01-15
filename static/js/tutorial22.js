a018bdce70d9e676a81a4446bd7cc1b942ec5151
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2016-01-15 17:25:51 +0000

    Templates for Jeremy's 2nd tutorial

---
+++ static/js/tutorial22.js
@@ -0,0 +1,385 @@
+// WV: actually, this is entirely generic
+// Main tutorial2 module.
+tutorial22 = {};
+tutorial22.url=window.location.href;
+tutorial22.hostname = '127.0.0.1';
+tutorial22.port = '4001';
+
+// A success hook which can be bound and rebound or set as null.
+tutorial22.successHook = null;
+
+// The current page number.
+tutorial22.currentPage = null;
+tutorial22.nPages = null;
+
+// Stdout state from the current IO evaluation.
+tutorial22.stdout = [];
+
+// Stdin state for the current IO evaluation.
+tutorial22.stdin = [];
+
+// IO expression.
+tutorial22.io = null;
+
+// WV extensions for HaskellMOOC
+// Allow continue on error
+tutorial22.continueOnError = false;
+// Handle context for equations
+tutorial22.equations=[];
+tutorial22.isEq = false;
+tutorial22.forget = function(varname) {
+//	alert('FORGET');
+	var remaining_eqs = [];
+         for (var i = 0; i <  tutorial22.equations.length; i++) {
+             var eq = tutorial22.equations[i];
+             var chunks = eq.split(/\s*=\s*/);
+             var lhs = chunks[0].trim(); // WV: but somehow there is still a trailing whitespace char after the varname in lhs!             
+//             alert('<'+lhs+'><'+varname+'>');
+             varre = new RegExp('\\b'+varname+'\\b');
+             if (!varre.test(lhs)) {
+//            	 alert('Pushing '+eq);
+            	 remaining_eqs.push(tutorial22.equations[i]);
+             }
+         }
+         tutorial22.equations=remaining_eqs;
+//         alert(varname+" => "+tutorial22.equations);
+}
+
+tutorial22.undo  = function() {
+    if (tutorial22.isEq) {
+     tutorial22.equations.pop();
+    }
+}
+
+
+// Files in the file system.
+tutorial22.files = {
+    "/hello": "Hello, World!",
+    "/files": "Your file system changes will stick around in your browser's local storage!",
+    "/welcome": "Welcome to your mini filesystem! Try playing with this function: getDirectoryContents",
+    "/functions": "You can also check out removeFile, writeFile, appendFile"
+};
+
+try {
+    if(typeof(Storage)!=="undefined")
+    {
+        tutorial22.files = (localStorage.files && JSON.parse(localStorage.files))
+            || tutorial22.files;
+    };
+} catch (e){ tutorial22.files = {} }
+
+tutorial22.showWarnings = function() {
+    !navigator.cookieEnabled     && $("#cookie-warning").show();
+    window['localStorage']==null && $("#storage-warning").show();
+}
+
+// A pre-command hook which can prevent the command from being run if
+// it returns true.
+tutorial22.preCommandHook = function(line,report){
+    var m, pages = tutorial22.pages.list;
+    tutorial22.nPages = pages.length;
+    tutorial22.isEq = false;
+    // if the line matches step{$n} then get page $n from  tutorial22.pages.list (i.e. pages)
+    if (m = line.trim().match(/^step([0-9]+)/)) {
+        var n = m[1] * 1;
+        if (n <= pages.length) {
+            tutorial22.setPage(n,null);
+            report();
+            return [true,'True'];
+        }
+    }
+    else if (m = line.trim().match(/^lesson([0-9]+)/)) {
+        // 'lesson' is simply an attribute of a page, a label to say to which lesson a page belongs.
+        var n = m[1] * 1;
+        for (var i = 0; i < pages.length; i++) {
+            if (pages[i].lesson == n) {
+                tutorial22.setPage(i,null);
+                report();
+                return [true,'True'];
+            }
+        }
+    } else if (line.trim() == 'next') {
+        if (tutorial22.currentPage < tutorial22.pages.list.length) {
+            tutorial22.setPage(tutorial22.currentPage + 1);
+        }
+        report();
+        return [true,'True'];
+    } else if (line.trim() == 'back' || line.trim() == 'prev') {
+        if (tutorial22.currentPage > 1) {
+            tutorial22.setPage(tutorial22.currentPage - 1);
+        }
+        report();
+        return [true,'True'];
+    } else if (line.trim() == 'help' || line.trim() == 'start' ) {
+        tutorial22.setPage(2,null);
+        report();
+        return [true,'True'];
+    } else if (/^undo/.test(line.trim()) ) {
+        tutorial22.undo();
+        report();        
+        return [true,'True'];
+    } else if (/^forget/.test(line.trim()) ) {
+        var chunks = line.trim().split(/\s+/);
+        var varname = chunks[1];
+        tutorial22.forget(varname);
+        report();
+        return [true,'True'];
+    }  else if (!/^let/.test( line.trim() ) && /^\w+(\s+\w+)*\s*=[^=\>\<]/.test( line.trim() ) ) {
+    	// This is an equation.     	
+        var nline = line.trim();
+        tutorial22.isEq = true;
+        tutorial22.equations.push(nline);
+        var context = '';
+        if (tutorial22.equations.length>0) {
+        	context = 'let {'+ tutorial22.equations.join(';') +' } in ';
+        }
+         var chunks = nline.split(/\s+=\s+/);
+         var lhs = chunks[0];
+         var rhs=chunks[1];
+         // Now, if the rhs is a lambda we should not return the lhs
+         // This is weak, because if I bind a lambda to f and then bind f to g, I'm still in trouble
+         var isLambda = false;
+         if (/\\/.test(rhs)) {
+        	 isLambda = true;      
+        	 alert("LAMBDA:"+rhs)
+         }
+         //lhs.replace(/\W+/g,'');
+         //rhs.replace(/\W+/g,'');
+//         alert('PRE:<'+lhs+'><'+rhs+'>');
+         // This is a naive check for recursion, we ignore the context
+         
+         var re = new RegExp('\\b'+lhs+'\\b');
+         if (re.test(rhs)) {
+//        	 alert('Recursion!');
+             tutorial22.equations.pop();
+             tutorial22.isEq = false;
+             line = 'let '+nline+' in '+lhs;
+         } else {
+             if (!isLambda) {
+		 line = context + lhs;
+             } else {
+        	 line = context + '"'+rhs+'"';
+             }
+         }
+        return [false,line];
+    } else {
+        // OK, an expression that is not an equation
+        line = 'let {'+ tutorial22.equations.join(';') +' } in '+line.trim();
+    }
+    return [false,line];
+};
+
+// Make the console controller.
+tutorial22.makeController = function(){
+    tutorial22.controller = $('#console').console({
+        promptLabel: 'λ ',
+        commandValidate: function(line){
+            if (line == "") return false;
+            else return true;
+        },
+        commandHandle: function(line,report){
+//            alert(tutorial.io);
+            if(tutorial22.io === null){
+                var retval = tutorial22.preCommandHook(line,report);
+                var ignoreCommand = retval[0];
+                var newLine = retval[1];
+                if(!ignoreCommand){
+                    tutorial22.ajaxCommand(newLine,report,[]);
+                }
+            } else {
+                tutorial22.stdin.push(line);
+                tutorial22.ajaxCommand(tutorial22.io,report,tutorial22.stdin);
+            }
+        },
+        autofocus: true,
+        animateScroll: true,
+        promptHistory: true,
+        welcomeMessage: 'Type Haskell expressions in here.',
+        continuedPromptLabel: '> '
+    });
+};
+
+// Make an AJAX command to the server with the given line.
+tutorial22.ajaxCommand = function(line,report,stdin){
+    var args = { 'exp': line,
+                 'args': JSON.stringify([stdin,tutorial22.files])
+               };
+    $.ajax({
+        url: '/eval',
+        dataType: 'json',
+        type: 'POST',
+        data: args,
+        success: function(result){
+            if(result.stdout !== undefined){
+ //               alert(result.stdout);
+                tutorial22.files = result.files;
+                result = result.stdout;
+                tutorial22.io = line;
+                var msgs = [];
+                if(result != null){
+                    for(var i = tutorial22.stdout.length; i < result.length; i++) {
+                        msgs.push({ msg: result[i], className: 'jquery-console-stdout' });
+                    }
+                }
+                tutorial22.stdout = result;
+                tutorial22.controller.continuedPrompt = true;
+                report(msgs);
+                tutorial22.controller.continuedPrompt = false;
+            } else {
+                if(result.error !== undefined){
+// A type error goes here,
+// What I want is the option to carry on
+                    result.expr = args.exp                    
+                    if (tutorial22.continueOnError) {
+                        if(tutorial22.successHook != null) {
+                            tutorial22.successHook(result);
+                        }
+                        report([{ msg: result.error || 'Unspecified error. Have you installed mueval?',
+                              className:'jquery-console-stdout' }]);
+                    } else {
+                    report([{ msg: result.error || 'Unspecified error. Have you installed mueval?',
+                              className:'jquery-console-error' }]);
+                    }
+                } else if(result.success){
+                    // So this is where we get when a computation just works
+                    result = result.success;
+                    var msgs = [];
+                    for(var i = tutorial22.stdout.length; i < result.stdout.length; i++) {
+                        msgs.push({ msg: result.stdout[i], className: 'jquery-console-stdout' });
+                    }
+                    if(tutorial22.successHook != null) {
+                        tutorial22.successHook(result);
+                    }
+                    if(result.type !== 'IO ()' && !result.value.match(/^</))
+                        msgs.push({ msg: result.value, className: 'jquery-console-value' });
+                    msgs.push({ msg: ':: ' + result.type, className: 'jquery-console-type' });
+                    report(msgs);
+                    tutorial22.files = result.files;
+                }
+                if (tutorial22.continueOnError) {
+ // nothing                        
+                } else {
+                    tutorial22.io = null;
+                    tutorial22.stdout = [];
+                    tutorial22.stdin = [];
+                }
+            }
+            if(typeof(Storage)!=="undefined")
+            {
+                localStorage.files = JSON.stringify(tutorial22.files);
+            }
+        }
+    });
+};
+
+// Make the guide on the rhs.
+tutorial22.makeGuide = function(){
+    var match = window.location.href.match(/#step([0-9]+)$/);
+    if(match){
+        tutorial22.setPage(match[1]*1,null);
+    } else {
+        tutorial22.setPage(1,null);
+    }
+};
+
+// Set the current page.
+tutorial22.setPage = function(n,result){
+    var page = tutorial22.pages.list[n-1];
+    if(page){
+        // Update the current page content
+        var guide = $('#guide');
+        var stepcounter = (tutorial22.currentPage != null) ? '<div style="color: grey; text-align:right">[step '+(tutorial22.currentPage+1)+'/'+tutorial22.nPages+']</div>' : '';
+        guide.html(stepcounter+(typeof page.guide == 'string'? page.guide : page.guide(result)));
+        tutorial22.makeGuidSamplesClickable();
+        // Update the location anchor
+        if (tutorial22.currentPage != null)
+            window.location = '/tutorial22'+'/#step' + n;
+        tutorial22.currentPage = n;
+        // Setup a hook for the next page
+        var nextPage = tutorial22.pages.list[n];
+        
+        if(nextPage) {
+            tutorial22.successHook = function(result){
+                if (nextPage.trigger &&
+                    nextPage.trigger(result))
+                    tutorial22.setPage(n+1,result);
+            };
+        }
+    } else {
+        throw "Unknown page number: " + n;
+    }
+};
+
+// Make the code examples in the guide clickable so that they're
+// inserted into the console.
+tutorial22.makeGuidSamplesClickable = function() {
+    $('#guide code').each(function(){
+        $(this).css('cursor','pointer');
+        $(this).attr('title','Click me to insert "' +
+                     $(this).text() + '" into the console.');
+        $(this).click(function(){
+            tutorial22.controller.promptText($(this).text());
+            tutorial22.controller.inner.click();
+        });
+    });
+}
+
+// Display the currently active users
+tutorial22.activeUsers = function(){
+    var active = $('.active-users');
+    // Tomorrow theme
+    var colors =
+        [// Tomorrow theme
+          "#f5871f" // Orange
+         ,"#eab700" // Yellow
+         ,"#718c00" // Green
+         ,"#3e999f" // Aqua
+         ,"#4271ae" // Blue
+         ,"#8959a8" // Purple
+         // Solarized theme
+         ,"#073642" // base02
+         ,"#586e75" // base01
+         ,"#b58900" // yellow
+         ,"#cb4b16" // orange
+         ,"#dc322f" // red
+         ,"#d33682" // magenta
+         ,"#6c71c4" // violet
+         ,"#268bd2" // blue
+         ,"#2aa198" // cyan
+         ,"#859900" // green
+        ]
+    var color_index = 0;
+    var color_cache = {};
+    function update(){
+        if(!$('.active-users').is(':visible')) return;
+        $.get('/users',function(users){
+            users = JSON.parse(users);
+            $('.active-users .user').remove();
+            var color;
+            for(var i = 0; i < users.length; i++){
+                if(typeof color_cache[users[i][0].toString()] != 'number') {
+                    color_cache[users[i][0].toString()] = color_index;
+                    color_index++;
+                }
+                color = colors[color_cache[users[i][0].toString()] % colors.length];
+                if (!color) color = colors[5];
+                active.append($('<div class="user"></div>').css('background-color',color));
+            }
+        });
+    }
+//    setInterval(update,5000);
+    update();
+};
+
+// Handy method.
+String.prototype.trim = function() {
+    return this.replace(/^[\t ]*(.*)[\t ]*$/,'$1');
+};
+
+// Main entry point.
+$(function(){
+    tutorial22.showWarnings();
+    tutorial22.makeController();
+    tutorial22.makeGuide();
+    tutorial22.activeUsers();
+});
---
+++ static/js/tutorial22.pages.js
@@ -0,0 +1,284 @@
+
+// Module for the guide pages
+tutorial22.pages = {};
+
+// Unshow a string
+tutorial22.pages.unString = function(str){
+    return str.replace(/^"(.*)"$/,"$1").replace(/\\"/,'"');
+}
+
+// Random message from a list of messages
+tutorial22.pages.rmsg = function(choices) {
+    return choices[Math.floor((Math.random()*100) % choices.length)];
+}
+
+// Simple HTML encoding
+tutorial22.pages.htmlEncode = function(text,shy){
+    var x = $('<div></div>');
+    x.text(text);
+    return x.html();
+}
+
+// Added by WV
+tutorial22.pages.isNum = function(result) {
+  var retval =
+    /(Num|Fractional|Integral|Floating)\s+[a-z]+\s+=>\s+[a-z]+/.test( result.type ) ||
+    /\(Ord\s+[a-z]+\s*,\s*(Num|Fractional|Integral|Floating)\s+[a-z]+\)\s+=>\s+[a-z]+/.test( result.type ) ||
+    /\((Num|Fractional|Integral|Floating)\s+[a-z]+\s*,\s*Ord\s+[a-z]+\)\s+=>\s+[a-z]+/.test( result.type ) ||
+    result.type == "Integer" ||
+    result.type == "Int" ||
+    result.type == "Bool";
+    return retval;
+}
+
+tutorial22.pages.isBool = function(result) {
+    return (result.type == "Bool");
+}
+
+
+    
+
+
+// All pages
+tutorial22.pages.list =
+    [
+      {title:'Haskell Interactive Tutorials',
+       guide:
+       '<div class="indent">' +
+       '<h3>Haskell Interactive Tutorials</h3>' +
+       //title="Click me to insert &quot;start&quot; into the console." style="cursor: pointer;"
+       '<p>In this environment you can try out Haskell code or take tutorials that guide you by prompting you to enter pieces of code and give you feedback on them. Each tutorial has its own url, e.g. for Tutorial 2 it is <a href="'+tutorial22.url+'">'+tutorial22.url+'</a>.</p>'+
+       '<br>'+
+       '<p>This coding environment does not offer all the functionality of the Haskell compiler <tt>ghc</tt> or the interactive Haskell interpreter <tt>ghci</tt>, because that would allow hackers to compromise your computer. Any feature that could potentially be a security risk has been disabled.</p>'+
+       '<p>Only <a href="https://hackage.haskell.org/package/pure-io-0.2.0/docs/PureIO.html#g:2">these</a> IO actions are supported in this app (more about this later in the course).</p>' +
+       '<p>Type <code>start</code>  at the <span style="color: purple">&#955;</span> prompt to start the tutorial.</p>' +
+       '<p>Or try typing any Haskell expression and see what happens.' +
+       '<small class="note">(click to insert)</small>:</p>' +
+       '<p>' +
+       '<code title="Click me to insert &quot;23 * 36&quot; into the console." style="cursor: pointer;">23 * 36</code> or <code title="Click me to insert &quot;reverse ' +
+       '&quot;hello&quot;&quot; into the console." style="cursor: pointer;">reverse ' +
+       '"hello"</code> or <code title="Click me to insert &quot;foldr (:) [] [1,2,3]&quot; into the console." style="cursor: pointer;">foldr (:) [] [1,2,3]</code> or <code title="Click me to insert." style="cursor: pointer;">do line <- getLine; putStrLn line</code> or <code>readFile "/welcome"</code>' +
+       '</p>' +
+
+       '</div>'
+      },
+        {title:'Tutorial 2.2: More Boolean Operations',
+         guide:
+         '<div class="indent">' +
+         '<h3>Tutorial 2.2: More Boolean Operations</h3>' +
+         '<p>We are going to look at some more boolean operations today.</p>'+
+         '<p>To go to the next step in the tutorial use <code>next</code>, to go back use <code>back</code>.</p>' +
+         '</div>'
+        },
+        ////////////////////////////////////////////////////////////////////////
+        // Lesson 1
+
+        // Simple boolean operators
+        {lesson:1,
+         title:'Boolean Negation',
+         guide:
+         '<h3>Boolean Negation</h3>'
+         + "<p>Boolean values are either True or False. True is the opposite of False, and vice verse. The not function returns the opposite boolean value, the logical complement. Try <code>not True</code></p>"
+        },
+        {
+          trigger:tutorial22.pages.isBool, 
+          guide:function(result){
+            if (!result) result = {expr:'not True',value:'False'};
+            var rexpr = result.expr.replace(/^let\s.+\sin\s+/, "");
+            var complied = /False/.test(result.value);            
+            var valid = /True|False/.test(result.value);
+
+	    var msg = "";
+            if (valid) {
+              if (complied) {
+                msg = "<p>You got back the boolean value "+result.value+" as expected.</p>";
+              } else {
+		var trimmedExpr = result.expr.replace(/^let\s.+\sin\s+/, "");
+                msg = '<p>OK, you calculated <code>' + trimmedExpr +'</code>,so you got back the truth value '+result.value+' as you would expect.</p>';
+              }
+            } else {
+                msg = '<p>'+
+                    "What you typed does not seem to be a simple boolean operation but it did something sensible!"
+                    +'</p>';
+		
+            }
+            tutorial22.continueOnError = true;
+	    var next_step = "<p>Now perform a double negation, e.g. <code> not (not False)</code> and observe that the final result is the same as the initial value.</p>";
+	      return msg + next_step;
+
+          }
+
+        },
+        // && and ||
+        {lesson:2,
+         title:'Boolean And',
+         trigger:function(result){
+	   bracketError = /applied\s+to\s+two\s+arguments/.test(result.error);
+	   booleanResult = result.type == "Bool";
+           return bracketError || booleanResult;
+	 },
+         guide:function(result){
+           var msg = "";
+	   if (/applied\s+to\s+two\s+arguments/.test(result.error)) {
+	       msg = msg + "<p>Remember to use brackets round the innermost not function call, since this must be evaluated first: <code>not (not True)</code></p>."
+	   }
+           var next_step =
+            "<h3>The And operator</h3>"
+            +"<p>Use the && infix operator as a boolean And. This only evaluates to True when both its inputs are True."
+            +"e.g. <code>True && True</code>. </p>";
+	   return msg + next_step;
+	 }
+        },
+
+   // FIXME - need a custom trigger to check we did an AND
+        
+        // Expression Syntax - cont'd
+        {
+         trigger:tutorial22.pages.isBool,
+          guide:function(result){
+	      tutorial22.continueOnError = true;
+            return "<p>You can apply these operations to other data types. Try comparing two Strings for equality, e.g. <code>\"hello\" == \"hola\"</code>.</p>";
+          }
+        },
+
+	{
+	 // this is a complex trigger - looking for an error or a correctly typed result!
+         trigger:function(result) {
+	     variableError = /^Not\s+in\s+scope/.test(result.error)
+	     missingQuoteError = /lexical\s+error\s+in\s+string/.test(result.error)
+	     booleanResult = result.type == "Bool";
+	     return variableError || missingQuoteError || booleanResult
+	 },
+         guide:function(result){
+	     msg = "";
+	     if (/^Not\s+in\s+scope/.test(result.error)) {
+		 msg = "<p>Did you forget to use double quotes \" \" around your strings? If so, Haskell thinks that you are referring to named values (like program variables)???"
+	     }
+	     if (/lexical\s+error\s+in\s+string/.test(result.error)) {
+		 msg = "<p>Did you forget a double quote \" at the beginning or end of one of your strings?"
+	     }
+             tutorial22.continueOnError = false;
+             return   msg + "<p> Now try String inequality <code>\"foo\" /= \"bar\"</code></p>";
+         }
+        },
+
+        {
+         trigger:tutorial22.pages.isBool,
+          guide:function(result){
+            return "<p>You can apply these operations to other data types. You might also try comparing two Bools directly, e.g. <code>True /= False</code>.</p>";
+          }
+        },
+
+        
+        
+        // Expression Syntax - cont'd
+        {
+          trigger:tutorial22.pages.isBool,
+          guide:function(result){
+            if (!result) result = {expr:'True/=False',value:'True'};
+            var complied = /Bool/.test(result.type);            
+            var rexpr = result.expr.replace(/^let\s.+\sin\s+/, "");
+            var valid = /True|False/.test(rexpr);
+            
+            var msg="";
+            if (valid) {
+                msg="<p>So this expression returned "+result.value+", illustrating the use of the equality test operator.</p>";
+            } 
+            var next_step =
+		  "<p>Now, what happens if you try to compare two values with different types? e.g. <code>True == 1</code>.";
+	      tutorial22.continueOnError = true;
+            return msg+next_step;
+          }
+        },
+
+        // Expression Syntax - Corner cases 2
+        {
+          trigger:function(result){
+	      var typeError =
+		  (/^\s+Couldn\'t\s+match\s+expected\s+type/.test(result.error)) || 
+		  (/^\s+No\s+instance\s+for/.test(result.error));
+              return typeError;
+          },
+          guide:function(result){
+          tutorial22.continueOnError = false;
+          return   "<p>As you can see, this equality test fails:  Haskell cannot compare two values that have <em>different types</em>. The full story is more complex, but for now, we can see that types <em>limit</em> the operations we can apply to particular values.</p>" +
+		  "<p>Haskell supports the standard comparison/relational operators, <, <=, >, >=. Try a simple comparison, e.g. <code>10 &gt; 9</p>";
+        }
+        },
+
+        // Expression Syntax - Corner cases 3
+        {
+          trigger:tutorial22.pages.isBool,
+          guide:function(result){
+          return  "<p>Note that relational operators also work on lists, in a dictionary-order manner (lexicographic). e.g. Try <code>[1,2,3] &lt; [1,2,3,4]</p>";
+        }
+        },
+
+	{
+	  trigger:tutorial22.pages.isBool,
+	  guide:function(result) {
+	    return "<p> Since strings are lists of characters in Haskell, we can do the same kinds of comparison operations on strings. Check whether Aadvark comes before Aaronic in the dictionary with this code: <code>&quot;Aardvark&quot; &lt; &quot;Aaronic&quot;";
+	  }
+	},
+// should we talk about string comparisons, 
+// about Eq class?
+
+        {
+          trigger:tutorial22.pages.isBool,
+          guide:function(result){
+          tutorial22.continueOnError = false;
+          var next_step = "<p>Now let's think about list membership. We want a boolean function that returns true if a value is part of a list, and false otherwise. This is the elem function. Try <code>elem 1 [1,2,3]</code></p>";
+              return next_step;
+	}
+        },
+
+	{
+	  trigger:tutorial22.pages.isBool,
+	  guide:function(result){
+            if (!result) result = {expr:'elem 1 [1,2,3]',value:'True'};
+            var complied = /Bool/.test(result.type);            
+            var rexpr = result.expr.replace(/^let\s.+\sin\s+/, "");
+            var valid = /True|False/.test(result.value);
+            var matches = rexpr.match(/^\s*elem\s+([0-9]+) (\[[^\]]*\])/);
+	    var element = matches[1];
+            var msg="<p>You see that element " + element + " is " + ((result.value)?"":" not ") + "part of the list.</p>";
+	    var next_step = msg + "<p>The elem function can be written infix, like an arithmetic operator, by enclosing its name in backquotes ``. Try <code>3 `elem` [1, 2, 3, 4, 5]</code>."
+            return next_step;
+	      
+	}
+	},
+	{
+	  trigger:tutorial22.pages.isBool,
+	  guide:function(result){
+	      var next_step ="<p>In fact, Haskell permits any two-argument function to be written as an infix operator using backquote characters. For a further example, try the max function as an infix operator: <code>42 `max` 13</code>";
+	      return next_step;
+          }
+        },
+
+	{
+	  trigger:tutorial22.pages.isNum,
+	  guide:function(result){
+	      var next_step = "<p>Also note that any Haskell infix operator, e.g. +, can be written as a prefix operator by enclosing it in parentheses, like <code>(+) 1 1</code>";
+	      return next_step;
+	  }
+	},
+
+{
+	  trigger:function(result){
+	    return true;
+	  },
+	  guide:function(result){
+		  
+	    var msg="<h3>And that's the end of Tutorial 2!</h3>" +
+	    		"<p>Well done, you finished another Haskell tutorial!.</p>" +
+	    		"<p>Let's recap what we've just discovered:</p>" +
+	            '<ul>'+
+	            '<li>Equality and Comparison Operators</li>' +
+		    '<li>Testing List membership with the elem function</li>' +
+                    '<li>Using infix and prefix operations.</li>' +
+	            '</ul></p>';
+
+	    return msg;
+	  },
+	},
+]
